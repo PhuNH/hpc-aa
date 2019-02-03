c1d184ec7c04b6ce7078c22d7a03091a95a4a2be
PhuNH <1079742+PhuNH@users.noreply.github.com>
2019-02-03 21:48:53 +0100

    Init exercise 6

---
+++ t6/src/examples/swe_wavepropagation.cpp
@@ -0,0 +1,289 @@
+/**
+ * @file
+ * This file is part of SWE.
+ *
+ * @author Alexander Breuer (breuera AT in.tum.de, http://www5.in.tum.de/wiki/index.php/Dipl.-Math._Alexander_Breuer)
+ *         Michael Bader (bader AT in.tum.de, http://www5.in.tum.de/wiki/index.php/Univ.-Prof._Dr._Michael_Bader)
+ *
+ * @section LICENSE
+ *
+ * SWE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SWE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SWE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @section DESCRIPTION
+ *
+ * Basic setting of SWE, which uses a wave propagation solver and an artificial or ASAGI scenario on a single block.
+ */
+
+#include <cassert>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include "../tools/help.hh"
+
+#include "../SWE_Block.hh"
+
+#ifndef CUDA
+#include "../SWE_WavePropagationBlock.hh"
+#else
+#include "../SWE_WavePropagationBlockCuda.hh"
+#endif
+
+#ifdef WRITENETCDF
+#include "writer/NetCdfWriter.hh"
+#else
+#include "writer/VtkWriter.hh"
+#endif
+
+#ifdef ASAGI
+#include "../scenarios/SWE_AsagiScenario.hpp"
+#else
+#include "../scenarios/SWE_simple_scenarios.h"
+#endif
+
+#ifdef READXML
+#include "../tools/CXMLConfig.hpp"
+#endif
+
+#ifndef STATICLOGGER
+#define STATICLOGGER
+#include "../tools/Logger.hpp"
+static tools::Logger s_sweLogger;
+#endif
+#include "tools/ProgressBar.hh"
+
+/**
+ * Main program for the simulation on a single SWE_WavePropagationBlock.
+ */
+int main( int argc, char** argv ) {
+  /**
+   * Initialization.
+   */
+  // check if the necessary command line input parameters are given
+  #ifndef READXML
+  if(argc != 4) {
+    std::cout << "Aborting ... please provide proper input parameters." << std::endl
+              << "Example: ./SWE_parallel 200 300 /work/openmp_out" << std::endl
+              << "\tfor a single block of size 200 * 300" << std::endl;
+    return 1;
+  }
+  #endif
+
+  //! number of grid cells in x- and y-direction.
+  int l_nX, l_nY;
+
+  //! l_baseName of the plots.
+  std::string l_baseName;
+
+  // read command line parameters
+  #ifndef READXML
+  l_nY = l_nX = atoi(argv[1]);
+  l_nY = atoi(argv[2]);
+  l_baseName = std::string(argv[3]);
+  #endif
+
+  // read xml file
+  #ifdef READXML
+  assert(false); //TODO: not implemented.
+  if(argc != 2) {
+    s_sweLogger.printString("Aborting. Please provide a proper input file.");
+    s_sweLogger.printString("Example: ./SWE_gnu_debug_none_augrie config.xml");
+    return 1;
+  }
+  s_sweLogger.printString("Reading xml-file.");
+
+  std::string l_xmlFile = std::string(argv[1]);
+  s_sweLogger.printString(l_xmlFile);
+
+  CXMLConfig l_xmlConfig;
+  l_xmlConfig.loadConfig(l_xmlFile.c_str());
+  #endif
+
+  #ifdef ASAGI
+  /* Information about the example bathymetry grid (tohoku_gebco_ucsb3_500m_hawaii_bath.nc):
+   *
+   * Pixel node registration used [Cartesian grid]
+   * Grid file format: nf = GMT netCDF format (float)  (COARDS-compliant)
+   * x_min: -500000 x_max: 6500000 x_inc: 500 name: x nx: 14000
+   * y_min: -2500000 y_max: 1500000 y_inc: 500 name: y ny: 8000
+   * z_min: -6.48760175705 z_max: 16.1780223846 name: z
+   * scale_factor: 1 add_offset: 0
+   * mean: 0.00217145586762 stdev: 0.245563641735 rms: 0.245573241263
+   */
+
+  //simulation area
+  float simulationArea[4];
+  simulationArea[0] = -450000;
+  simulationArea[1] = 6450000;
+  simulationArea[2] = -2450000;
+  simulationArea[3] = 1450000;
+
+  SWE_AsagiScenario l_scenario( ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_bath.nc",
+                                ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_displ.nc",
+                                (float) 28800., simulationArea);
+  #else
+  // create a simple artificial scenario
+  SWE_BathymetryDamBreakScenario l_scenario;
+  #endif
+
+  //! number of checkpoints for visualization (at each checkpoint in time, an output file is written).
+  int l_numberOfCheckPoints = 20;
+
+  //! size of a single cell in x- and y-direction
+  float l_dX, l_dY;
+
+  // compute the size of a single cell
+  l_dX = (l_scenario.getBoundaryPos(BND_RIGHT) - l_scenario.getBoundaryPos(BND_LEFT) )/l_nX;
+  l_dY = (l_scenario.getBoundaryPos(BND_TOP) - l_scenario.getBoundaryPos(BND_BOTTOM) )/l_nY;
+
+  // initialize the grid data and the corresponding static variables
+  SWE_Block::initGridData(l_nX,l_nY,l_dX,l_dY);
+
+  //! origin of the simulation domain in x- and y-direction
+  float l_originX, l_originY;
+
+  // get the origin from the scenario
+  l_originX = l_scenario.getBoundaryPos(BND_LEFT);
+  l_originY = l_scenario.getBoundaryPos(BND_BOTTOM);
+
+  // create a single wave propagation block
+  #ifndef CUDA
+  SWE_WavePropagationBlock l_wavePropgationBlock;
+  #else
+  SWE_WavePropagationBlockCuda l_wavePropgationBlock;
+  #endif
+
+  // initialize the wave propgation block
+  l_wavePropgationBlock.initScenario(l_originX, l_originY, l_scenario);
+
+
+  //! time when the simulation ends.
+  float l_endSimulation = l_scenario.endSimulation();
+
+  //! checkpoints when output files are written.
+  float* l_checkPoints = new float[l_numberOfCheckPoints+1];
+
+  // compute the checkpoints in time
+  for(int cp = 0; cp <= l_numberOfCheckPoints; cp++) {
+     l_checkPoints[cp] = cp*(l_endSimulation/l_numberOfCheckPoints);
+  }
+
+  // Init fancy progressbar
+  tools::ProgressBar progressBar(l_endSimulation);
+
+  // write the output at time zero
+  s_sweLogger.printOutputTime((float) 0.);
+  progressBar.update(0.);
+
+  std::string l_fileName = generateBaseFileName(l_baseName,0,0);
+  //boundary size of the ghost layers
+  io::BoundarySize l_boundarySize = {1};
+#ifdef WRITENETCDF
+  //construct a NetCdfWriter
+  io::NetCdfWriter l_writer( l_fileName,
+		  l_wavePropgationBlock.getBathymetry(),
+		  l_boundarySize,
+		  l_nX, l_nY,
+		  l_dX, l_dY,
+		  l_originX, l_originY);
+#else
+  // consturct a VtkWriter
+  io::VtkWriter l_writer( l_fileName,
+		  l_wavePropgationBlock.getBathymetry(),
+		  l_boundarySize,
+		  l_nX, l_nY,
+		  l_dX, l_dY );
+#endif
+  // Write zero time step
+  l_writer.writeTimeStep( l_wavePropgationBlock.getWaterHeight(),
+                          l_wavePropgationBlock.getDischarge_hu(),
+                          l_wavePropgationBlock.getDischarge_hv(),
+                          (float) 0.);
+
+
+  /**
+   * Simulation.
+   */
+  // print the start message and reset the wall clock time
+  progressBar.clear();
+  s_sweLogger.printStartMessage();
+  s_sweLogger.initWallClockTime(time(NULL));
+
+  //! simulation time.
+  float l_t = 0.0;
+  progressBar.update(l_t);
+
+  // loop over checkpoints
+  for(int c=1; c<=l_numberOfCheckPoints; c++) {
+    // reset the cpu clock
+    s_sweLogger.resetCpuClockToCurrentTime();
+
+    // do time steps until next checkpoint is reached
+    while( l_t < l_checkPoints[c] ) {
+      // set values in ghost cells:
+      l_wavePropgationBlock.setGhostLayer();
+      
+      // approximate the maximum time step
+      // TODO: This calculation should be replaced by the usage of the wave speeds occuring during the flux computation
+      // Remark: The code is executed on the CPU, therefore a "valid result" depends on the CPU-GPU-synchronization.
+//      l_wavePropgationBlock.computeMaxTimestep();
+
+      // compute numerical flux on each edge
+      l_wavePropgationBlock.computeNumericalFluxes();
+
+      //! maximum allowed time step width.
+      float l_maxTimeStepWidth = l_wavePropgationBlock.getMaxTimestep();
+
+      // update the cell values
+      l_wavePropgationBlock.updateUnknowns(l_maxTimeStepWidth);
+
+      // update simulation time with time step width.
+      l_t += l_maxTimeStepWidth;
+
+      // print the current simulation time
+      progressBar.clear();
+      s_sweLogger.printSimulationTime(l_t);
+      progressBar.update(l_t);
+    }
+
+    // update the cpu time in the logger
+    s_sweLogger.updateCpuTime();
+
+    // print current simulation time of the output
+    progressBar.clear();
+    s_sweLogger.printOutputTime(l_t);
+    progressBar.update(l_t);
+
+    // write output
+    l_writer.writeTimeStep( l_wavePropgationBlock.getWaterHeight(),
+                            l_wavePropgationBlock.getDischarge_hu(),
+                            l_wavePropgationBlock.getDischarge_hv(),
+                            l_t);
+  }
+
+  /**
+   * Finalize.
+   */
+  // write the statistics message
+  progressBar.clear();
+  s_sweLogger.printStatisticsMessage();
+
+  // print the cpu time
+  s_sweLogger.printCpuTime();
+
+  // print the wall clock time (includes plotting)
+  s_sweLogger.printWallClockTime(time(NULL));
+
+  return 0;
+}
---
+++ t6/src/opengl/controller.cpp
@@ -0,0 +1,367 @@
+// =====================================================================
+// This file is part of SWE_CUDA (see file SWE_Block.cu for details).
+// 
+// Copyright (C) 2010,2011 Tobias Schnabel
+// Copyright (C) 2012      Sebastian Rettenberger
+// 
+// SWE_CUDA is free software: you can redristribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SWE_CUDA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with SWE_CUDA.  If not, see <http://www.gnu.org/licenses/>.
+// =====================================================================
+#include "controller.h"
+
+#include "../scenarios/SWE_simple_scenarios.h"
+#include "../scenarios/SWE_simple_scenarios_vis.h"
+#ifdef ASAGI
+#include "../scenarios/SWE_AsagiScenario.hpp"
+#include "../scenarios/SWE_AsagiScenario_vis.hpp"
+#endif // ASAGI
+
+/**
+    Constructor
+
+	@param sim				instance of simulation class
+	@param vis				instance of visualization class
+
+*/	
+Controller::Controller(Simulation* sim, Visualization* vis) {
+	simulation = sim;
+	visualization = vis;
+	isActive = true;
+	done = false;
+	paused = false;
+	allowStep = false;
+
+	// No scenario loaded
+	memset(scenarios, 0, SCENARIO_COUNT*sizeof(SWE_Scenario*));
+	memset(visInfos, 0, SCENARIO_COUNT*sizeof(SWE_VisInfo*));
+}
+
+Controller::~Controller()
+{
+	// Delete scenarios
+	for (int i = 0; i < SCENARIO_COUNT; i++) {
+		delete scenarios[i];
+		delete visInfos[i];
+	}
+}
+
+/**
+    Process all user events in a loop
+	Returns true, when user wants to quit
+
+*/	
+bool Controller::handleEvents() {
+	// Handle events
+	SDL_Event event;
+	allowStep = false;
+
+	while ( SDL_PollEvent(&event) ) 
+	{
+		switch( event.type )
+		{
+		case SDL_MOUSEMOTION:
+			if (event.motion.state & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+				// Left mouse button dragging
+				visualization->camera->orient(0.5f*event.motion.yrel, 0.5f*event.motion.xrel);
+			}
+			else if (event.motion.state & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
+				// Panning happens here
+				visualization->camera->panning(event.motion.x, event.motion.y);			
+			}
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			// Scroll wheel down
+			if ( event.button.button == SDL_BUTTON_WHEELDOWN ) {
+				// Zoom out
+				visualization->camera->zoomOut(1.2f);
+			} else if (event.button.button == SDL_BUTTON_RIGHT) {
+				visualization->camera->startPanning(event.button.x, event.button.y);
+			break;
+		case SDL_MOUSEBUTTONUP:
+			// Scroll wheel up
+			if ( event.button.button == SDL_BUTTON_WHEELUP ) {
+				// Zoom in
+				visualization->camera->zoomIn(1.15f);
+			}
+			} else if (event.button.button == SDL_BUTTON_MIDDLE) {
+				visualization->camera->reset();
+			}
+			break;
+		case SDL_ACTIVEEVENT:
+			// Don't draw anything if we're getting minimized
+			if ( event.active.state & SDL_APPACTIVE ) {
+				isActive = (event.active.gain != 0);	
+			}
+			break;			    
+		case SDL_VIDEORESIZE:
+			// Our window was resized
+			visualization->resizeWindow(event.resize.w, event.resize.h);
+			break;
+		case SDL_KEYDOWN:
+			// User has pressed a key
+			done = handleKeyPress( &event.key.keysym);
+			break;
+		case SDL_QUIT:
+			// Window close request
+			done = true;
+			break;
+		default:
+			break;
+		}
+	}
+	return done;
+}
+
+/**
+	Returns true, when window has focus 
+
+*/	
+bool Controller::hasFocus() {
+	return isActive && !done;
+}
+
+/**
+    Return whether program is currently paused
+
+*/	
+bool Controller::isPaused() {
+	return paused && !allowStep;
+}
+
+/**
+    Process single keyboard event
+	@param *keysym			pointer to the sdl keyevent structure
+
+	@todo Refector!!
+
+*/	
+bool Controller::handleKeyPress( SDL_keysym *keysym) {
+
+	switch ( keysym->sym ) {
+		case SDLK_ESCAPE:
+			// ESC key was pressed -> exit
+			return true;
+			break;
+		case SDLK_r:
+			// Restart simulation
+			allowStep = paused;
+			simulation->restart();
+			break;
+		case SDLK_w:
+			// Switch rendering mode
+			visualization->toggleRenderingMode();
+			break;
+		case SDLK_s:
+			// Save simulation data to file
+			simulation->saveToFile();
+			break;
+		case SDLK_RIGHT:
+			// Advance single timestep when paused
+			allowStep = paused;
+			break;
+		case SDLK_SPACE:
+			// Pause/Resume
+			paused = !paused;
+			break;
+		case SDLK_PLUS:
+			// Increase water scaling
+			visualization->modifyWaterScaling(1.5);
+			break;
+		case SDLK_MINUS:
+			// Decrease water scaling
+			visualization->modifyWaterScaling(1/1.5);
+			break;
+		case SDLK_1:
+			// Load scenario 1
+			{
+			  allowStep = paused;
+
+			  if (scenarios[0] == 0)
+				  scenarios[0] = new SWE_RadialDamBreakScenario;
+
+			  SWE_Scenario* newScene = scenarios[0];
+
+			  // define grid size and initial time step
+			  float dx = (newScene->getBoundaryPos(BND_RIGHT) - newScene->getBoundaryPos(BND_LEFT) )/SWE_Block::getNx();
+			  float dy = (newScene->getBoundaryPos(BND_TOP) - newScene->getBoundaryPos(BND_BOTTOM) )/SWE_Block::getNy();
+			  SWE_Block::initGridData(SWE_Block::getNx(),SWE_Block::getNy(),dx,dy);
+
+			  simulation->loadNewScenario(newScene);
+			  visualization->init(*simulation);
+			}
+			break;
+		case SDLK_2:
+			// Load scenario 2
+			{
+			  allowStep = paused;
+
+			  if (scenarios[1] == 0) {
+				  scenarios[1] = new SWE_BathymetryDamBreakScenario();
+				  visInfos[1] = new SWE_BathymetryDamBreakVisInfo();
+			  }
+
+			  SWE_Scenario* newScene = scenarios[1];
+
+			  // define grid size and initial time step
+			  float dx = (newScene->getBoundaryPos(BND_RIGHT) - newScene->getBoundaryPos(BND_LEFT) )/SWE_Block::getNx();
+			  float dy = (newScene->getBoundaryPos(BND_TOP) - newScene->getBoundaryPos(BND_BOTTOM) )/SWE_Block::getNy();
+			  SWE_Block::initGridData(SWE_Block::getNx(),SWE_Block::getNy(),dx,dy);
+
+			  simulation->loadNewScenario(newScene);
+			  visualization->init(*simulation, visInfos[1]);
+			}
+			break;
+		case SDLK_3:
+			// Load scenario 3
+			{
+			  allowStep = paused;
+
+			  if (scenarios[2] == 0)
+				  scenarios[2] = new SWE_SplashingPoolScenario();
+
+			  SWE_Scenario* newScene = scenarios[2];
+
+			  // define grid size and initial time step
+			  float dx = (newScene->getBoundaryPos(BND_RIGHT) - newScene->getBoundaryPos(BND_LEFT) )/SWE_Block::getNx();
+			  float dy = (newScene->getBoundaryPos(BND_TOP) - newScene->getBoundaryPos(BND_BOTTOM) )/SWE_Block::getNy();
+			  SWE_Block::initGridData(SWE_Block::getNx(),SWE_Block::getNy(),dx,dy);
+
+			  simulation->loadNewScenario(newScene);
+			  visualization->init(*simulation);
+			}
+			break;
+#ifdef ASAGI
+		case SDLK_4:
+			// Load scenario 4
+			{
+				allowStep = paused;
+
+				if (scenarios[3] == 0) {
+					//simulation area
+					float simulationArea[4];
+					simulationArea[0] = -450000;
+					simulationArea[1] = 6450000;
+					simulationArea[2] = -2450000;
+					simulationArea[3] = 1450000;
+					scenarios[3] = new SWE_AsagiScenario(
+							ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_bath.nc",
+				            ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_displ.nc",
+				            (float) 28800., simulationArea);
+				}
+
+				SWE_Scenario* newScene = scenarios[3];
+
+				  // define grid size and initial time step
+				  float dx = (newScene->getBoundaryPos(BND_RIGHT) - newScene->getBoundaryPos(BND_LEFT) )/SWE_Block::getNx();
+				  float dy = (newScene->getBoundaryPos(BND_TOP) - newScene->getBoundaryPos(BND_BOTTOM) )/SWE_Block::getNy();
+				  SWE_Block::initGridData(SWE_Block::getNx(),SWE_Block::getNy(),dx,dy);
+
+				  simulation->loadNewScenario(newScene);
+				  visualization->init(*simulation);
+			}
+			break;
+		case SDLK_5:
+			// Load scenario 5
+			{
+				allowStep = paused;
+
+				if (scenarios[4] == 0) {
+					//simulation area
+					float simulationArea[4];
+					simulationArea[0] = -450000;
+					simulationArea[1] = 700000;
+					simulationArea[2] = -1000000;
+					simulationArea[3] = 1450000;
+					scenarios[4] = new SWE_AsagiScenario(
+							ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_bath.nc",
+				            ASAGI_INPUT_DIR "tohoku_gebco_ucsb3_500m_hawaii_displ.nc",
+				            (float) 28800., simulationArea);
+
+					visInfos[4] = new SWE_AsagiJapanSmallVisInfo();
+				}
+
+				SWE_Scenario* newScene = scenarios[4];
+
+				  // define grid size and initial time step
+				  float dx = (newScene->getBoundaryPos(BND_RIGHT) - newScene->getBoundaryPos(BND_LEFT) )/SWE_Block::getNx();
+				  float dy = (newScene->getBoundaryPos(BND_TOP) - newScene->getBoundaryPos(BND_BOTTOM) )/SWE_Block::getNy();
+				  SWE_Block::initGridData(SWE_Block::getNx(),SWE_Block::getNy(),dx,dy);
+
+				  simulation->loadNewScenario(newScene);
+				  visualization->init(*simulation, visInfos[4]);
+			}
+			break;
+		case SDLK_6:
+			// Load scenario 6
+			{
+				allowStep = paused;
+
+				if (scenarios[5] == 0) {
+					//simulation area
+					float simulationArea[4];
+					simulationArea[0] = -13775000;
+					simulationArea[1] = 1655000;
+					simulationArea[2] = -2765000;
+					simulationArea[3] = 8870000;
+					scenarios[5] = new SWE_AsagiScenario(
+							ASAGI_INPUT_DIR "chile_gebco_usgs_500m_bath.nc",
+				            ASAGI_INPUT_DIR "chile_gebco_usgs_500m_displ.nc",
+				            (float) 28800., simulationArea);
+				}
+
+				SWE_Scenario* newScene = scenarios[5];
+
+				  // define grid size and initial time step
+				  float dx = (newScene->getBoundaryPos(BND_RIGHT) - newScene->getBoundaryPos(BND_LEFT) )/SWE_Block::getNx();
+				  float dy = (newScene->getBoundaryPos(BND_TOP) - newScene->getBoundaryPos(BND_BOTTOM) )/SWE_Block::getNy();
+				  SWE_Block::initGridData(SWE_Block::getNx(),SWE_Block::getNy(),dx,dy);
+
+				  simulation->loadNewScenario(newScene);
+				  visualization->init(*simulation);
+			}
+			break;
+		case SDLK_7:
+			// Load scenario 7
+			{
+				allowStep = paused;
+
+				if (scenarios[6] == 0) {
+					//simulation area
+					float simulationArea[4];
+					simulationArea[0] = -2275000;
+					simulationArea[1] = 1655000;
+					simulationArea[2] = -2265000;
+					simulationArea[3] = 1870000;
+					scenarios[6] = new SWE_AsagiScenario(
+							ASAGI_INPUT_DIR "chile_gebco_usgs_500m_bath.nc",
+				            ASAGI_INPUT_DIR "chile_gebco_usgs_500m_displ.nc",
+				            (float) 28800., simulationArea);
+				}
+
+				SWE_Scenario* newScene = scenarios[6];
+
+				  // define grid size and initial time step
+				  float dx = (newScene->getBoundaryPos(BND_RIGHT) - newScene->getBoundaryPos(BND_LEFT) )/SWE_Block::getNx();
+				  float dy = (newScene->getBoundaryPos(BND_TOP) - newScene->getBoundaryPos(BND_BOTTOM) )/SWE_Block::getNy();
+				  SWE_Block::initGridData(SWE_Block::getNx(),SWE_Block::getNy(),dx,dy);
+
+				  simulation->loadNewScenario(newScene);
+				  visualization->init(*simulation);
+			}
+			break;
+#endif // ASAGI
+		default:
+			break;
+	}
+	return false;
+}
